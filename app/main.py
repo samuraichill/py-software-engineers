cc25c4d6725e96651e0ed87ce45089999f36360e
Volodymyr <gurevvova352@gmail.com>
2026-09-12 15:42:25 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -34,7 +34,7 @@ class AndroidDeveloper(SoftwareEngineer):
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = ["Java", "Android studio"]
+        self.skills.extend(["Java", "Android studio"])
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
